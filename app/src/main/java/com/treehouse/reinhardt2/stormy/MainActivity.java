9b71788b99b35de805dc111432fcc1442ec1d4c1
Judy Reinhardt <judy@eicode.com>
2015-08-14 16:12:51 -0700

    User Permission for Internet Added

--- app/src/main/java/com/treehouse/reinhardt2/stormy/MainActivity.java
+++ app/src/main/java/com/treehouse/reinhardt2/stormy/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
@@ -31,13 +32,24 @@ public class MainActivity extends ActionBarActivity {
                 .build();
 
         Call call = client.newCall(request);
-        try {
-            Response response = call.execute();
-            if (response.isSuccessful()) {
-                Log.v(TAG, response.body().string());
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
             }
-        } catch (IOException e) {
-            Log.e(TAG, "Exception caught: ", e);
-        }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                try {
+                    if (response.isSuccessful()) {
+                        Log.v(TAG, response.body().string());
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Exception caught: ", e);
+                }
+            }
+        });
+
+        Log.d(TAG, "Main UI code is running!");
     }
 }
